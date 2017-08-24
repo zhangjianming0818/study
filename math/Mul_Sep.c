1a385726abec78eaa185f47d312038b0171a2b5e
zhangjianming0818 <zjm644276040@163.com>
2017-08-24 18:53:06 +0800

    Description:updata Mul_Sep.c Project    :math Author     :zhangjianming modify     :add new Multipy method feature/bugfix:feature

--- math/Mul_Sep.c
+++ math/Mul_Sep.c
@@ -20,22 +20,20 @@
 #define LOW_BIT 15
 #define LOW_BIT_MASK 0x80007fff      //Low 15 bit
 #define H_MUL_BIT 30                 //15*2
-#define SIG_BIT_MASK 0x80000000
+#define SIG_32BIT_MASK 0x80000000
 
-int32_t Multiply_Separate(int32_t src1,int32_t src2,int32_t Exp_Bit)
+int32_t Multiply_Separate1(int32_t src1,int32_t src2,int32_t Exp_Bit)
 {
    int sig_bit = 1;
-   if((src1&SIG_BIT_MASK)^(src2&SIG_BIT_MASK))
+   if((src1&SIG_32BIT_MASK)^(src2&SIG_32BIT_MASK))
     sig_bit = -1;                                   //get the sign after multiply
 
    src1 = labs(src1);                               //get absolute value 
    src2 = labs(src2);
 
-   //printf("src %08x\n",src1);
    int32_t HightSrc1 = src1 >> LOW_BIT;            //get Hight bit 
    int32_t LowSrc1   = src1&LOW_BIT_MASK;          // get low bit
-   //printf("Hight %08x\n",HightSrc1);
-   //printf("Low %08x\n",LowSrc1);
+
    int32_t HightSrc2 = src2 >> LOW_BIT;
    int32_t LowSrc2   = src2&LOW_BIT_MASK;
    
@@ -58,17 +56,48 @@ int32_t Multiply_Separate(int32_t src1,int32_t src2,int32_t Exp_Bit)
     return result;
 }
 
+#define LOW_BIT_MASK2 0x00007fff      //Low 15 bit
+#define SIG_64BIT_MASK 0x8000000000000000
+int64_t Multiply_Separate2(int32_t src1,int32_t src2,int32_t Exp_Bit)
+{
+   
+   int64_t sum=0;
+   int32_t HightSrc1 = src1 >> LOW_BIT;            //get Hight bit 
+   int32_t LowSrc1   = src1&LOW_BIT_MASK2;          // get low bit
 
+   int32_t HightSrc2 = src2 >> LOW_BIT;
+   int32_t LowSrc2   = src2&LOW_BIT_MASK2;
+   
+   int32_t HighMul  = HightSrc1*HightSrc2;                     //Hight bit multiply
+   int32_t MidMul   = HightSrc1*LowSrc2 + HightSrc2*LowSrc1;   // Hight * Low bit
+   int32_t LowMul   = LowSrc1*LowSrc2;                         //low bit multiply
+
+   //printf("%d %d %d \n",HighMul,MidMul,LowMul);
+   
+   sum = HighMul;
+   sum = sum << LOW_BIT;
+   sum += MidMul;
+   sum  = sum << LOW_BIT;
+   sum += LowMul;
+   //printf("sum = %ld \n",sum);
+   if((sum<0) && sum > -(1<<Exp_Bit))
+      sum =0;
+   else
+    sum  = sum >> Exp_Bit;
+    return sum;
+}
 
 void main(void)
 {
-    int32_t i,ret;
+    int64_t i,ret;
     struct timeval tv1,tv2;
     //gettimeofday(&tv1,NULL);
-    for(i=1048576;i<33554432;i++){
-       ret = Multiply_Separate(-i,i,20);
-       printf("%d \n",ret);
-    }
+    //for(i=(1<<24);i<(1<<28);i++){
+       //ret = Multiply_Separate1(-i,i,20);
+       //printf("%08x \n",ret);
+       ret = Multiply_Separate2(1444338,1444338,20);
+        printf("%ld \n",ret);
+    //}
     //gettimeofday(&tv2,NULL);
     //printf("%d :%d \n",tv2.tv_sec-tv1.tv_sec,tv2.tv_usec-tv1.tv_usec);
 
